454a9ffe96823f34f035b966c92b98da65b48501
Oğuz Yıldırım <oguzyildirimyo@icloud.com>
2025-05-22 17:09:45 +0300

    🐸 [ADD] Network layer has been added to the project.

---
+++ BlindIDCase/Network/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  BlindIDCase
+//
+//  Created by Oguz Yildirim on 22.05.2025.
+//
+
+import Foundation
+
+enum API {
+    static var baseURL: String {
+        return "https://moviatask.cerasus.app"
+    }
+}
---
+++ BlindIDCase/Network/APIHTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  APIHTTPMethod.swift
+//  BlindIDCase
+//
+//  Created by Oguz Yildirim on 22.05.2025.
+//
+
+import Foundation
+
+enum APIHTTPMethod: String {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+    case PATCH
+}
