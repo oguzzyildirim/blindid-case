795a66c4db4b88a41503d113d0bf7789b1f180ae
Oğuz Yıldırım <oguzyildirimyo@icloud.com>
2025-05-24 17:32:07 +0300

    🐸 [ADD] Image extensions has been added.

---
+++ BlindIDCase/Extensions/Image+Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  Image+Extensions.swift
+//  BlindIDCase
+//
+//  Created by Oguz Yildirim on 23.05.2025.
+//
+
+
+import SwiftUI
+
+extension Image {
+    static func fromURL(_ urlString: String, placeholder: Image = Image(systemName: "photo")) -> some View {
+        AsyncImage(
+            url: URL(string: urlString),
+            content: { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            },
+            placeholder: {
+                placeholder
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.gray.opacity(0.3))
+            }
+        )
+    }
+} 
