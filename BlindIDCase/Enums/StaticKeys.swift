49e34b8ec22637d6b8b9c1d3d2fc99b89ca9d5d6
Oğuz Yıldırım <oguzyildirimyo@icloud.com>
2025-05-25 19:07:06 +0300

    🐸 [ADD] FavoriteTabStrings has been added to the project.

--- BlindIDCase/Enums/StaticKeys.swift
+++ BlindIDCase/Enums/StaticKeys.swift
@@ -9,10 +9,12 @@ import Foundation
 
 enum StaticKeys {
     case currentTab
+    case loginStatus
     
     var key: String {
         switch self {
             case .currentTab: return "currentTab"
+            case .loginStatus: return "loginStatus"
         }
     }
 }
