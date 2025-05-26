037e97e7fc866333d000ff94f97e7baf5a0d1c5b
Oğuz Yıldırım <oguzyildirimyo@icloud.com>
2025-05-26 04:25:01 +0300

    🐸 [ADD] CustomNavigationBar, CustomPlaceHolder and ProfileUpdate view have been added to the project.

---
+++ BlindIDCase/Generics/CustomPlaceHolder.swift
@@ -0,0 +1,18 @@
+//
+//  CustomPlaceHolder.swift
+//  BlindIDCase
+//
+//  Created by Oguz Yildirim on 26.05.2025.
+//
+
+import SwiftUI
+
+struct CustomPlaceHolder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomPlaceHolder()
+}
